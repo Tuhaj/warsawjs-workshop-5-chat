1e633bbd2cdbd9d4a188b35597e37d6b5d0d2aa3
Tuhaj <zrazic@wp.pl>
2017-04-23 15:34:48 +0200

    adds registration and login

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const socket = io('http://localhost:3000');
+const socket = io(`http://${process.argv[2] || 'localhost'}:3000`);
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
@@ -8,20 +8,52 @@ const readline = require('readline').createInterface({
 
 console.log('Client started!');
 
-socket.emit('message', "Hello Universe");
 socket.on('message', (msg) => {
   clearPrompt();
   console.log(`>> ${msg}`);
   readline.prompt();
 });
 
+socket.on('register', (registerSuccess) => {
+  clearPrompt();
+  if (registerSuccess) {
+    console.log('>> REGISTRATION SUCCESSFUL');
+  } else {
+    console.log('>> REGISTRATION FAILED');
+  }
+  readline.prompt();
+})
 
 readline.on('line', (line) => {
-  if (line.trim()) {
+  const lineArgs = line.split(/\s+/);
+  const firstWord = line.split(/\s+/)[0];
+  if (firstWord === '/exit') {
+    readline.close();
+    process.exit(0);
+  } else if (firstWord === '/register' && lineArgs.length >= 3) {
+    socket.emit('register', {
+      username: lineArgs[1],
+      password: lineArgs[2]
+    });
+  } else if (firstWord === '/login' && lineArgs.length >= 3) {
+    socket.emit('login', {
+      username: lineArgs[1],
+      password: lineArgs[2]
+    });
+  } else if (line.trim()) {
     socket.emit('message', line);
   }
   readline.prompt();
 });
+
+socket.on('login', (username) => {
+  clearPrompt();
+  if (username) {
+    readline.setPrompt(`${username}: `);
+  } else {
+    console.log('>> LOGIN FAILED');
+  }
+})
 
 function clearPrompt() {
   process.stdout.cursorTo(0);
--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const io = require('socket.io');
 const socket = io();
+const USERS = [];
 
 socket.on('connection', (client) => {
   console.log(`client with id: ${client.id}, connected!`);
   client.on('message', (msg) => {
     console.log(msg);
     client.broadcast.emit('message', msg);
+  })
+  client.on('register', (userObject) => {
+    if (USERS[userObject.username]) {
+      client.emit('register', false);
+    } else {
+      USERS[userObject.username] = {
+        password: userObject.password,
+        logged_in: false
+      };
+      client.emit('register', true);
+    };
+  })
+  client.on('login', (userObject) => {
+    const user = USERS[userObject.username];
+    if (user.password === userObject.password) {
+      user.logged_in = true;
+      client.emit('login', userObject.username);
+    } else {
+      client.emit('login', false);
+    }
   })
 });
 socket.listen(3000);
