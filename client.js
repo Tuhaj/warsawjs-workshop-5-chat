49c35625ae2ade8f93f7d137b20a2d8ef5ea8fe0
Tuhaj <zrazic@wp.pl>
2017-04-23 12:54:32 +0200

    adds prompt

--- client.js
+++ client.js
@@ -1,6 +1,21 @@
 const io = require('socket.io-client');
 const socket = io('http://localhost:3000');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 
 console.log('Client started!');
 
 socket.emit('message', "Hello Universe");
+socket.on('message', (msg) => {
+  console.log(`from server: ${msg}`);
+});
+
+
+readline.on('line', (line) => {
+  socket.emit('message', line);
+});
+
+readline.prompt();
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ socket.on('connection', (client) => {
   console.log(`client with id: ${client.id}, connected!`);
   client.on('message', (msg) => {
     console.log(msg);
+    client.broadcast.emit('message', msg);
   })
 });
 socket.listen(3000);
