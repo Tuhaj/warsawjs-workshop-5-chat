d3119c79909edabe4f12015676aebb615c72659c
Tuhaj <zrazic@wp.pl>
2017-04-23 12:35:23 +0200

    adds hello world message

--- client.js
+++ client.js
@@ -2,3 +2,5 @@ const io = require('socket.io-client');
 const socket = io('http://localhost:3000');
 
 console.log('Client started!');
+
+socket.emit('message', "Hello Universe");
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const socket = io();
 
 socket.on('connection', (client) => {
   console.log(`client with id: ${client.id}, connected!`);
+  client.on('message', (msg) => {
+    console.log(msg);
+  })
 });
 socket.listen(3000);
 
