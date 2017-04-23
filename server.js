05084f97b69bf2bc5d2e3d52c803a9004396376c
Tuhaj <zrazic@wp.pl>
2017-04-23 12:25:02 +0200

    adds base for client

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const io = require('socket.io');
 const socket = io();
 
 socket.on('connection', (client) => {
-  console.log("client", client);
   console.log(`client with id: ${client.id}, connected!`);
 });
 socket.listen(3000);
